38306990be6bcbb93633591eaa7e777b3d2bd05d
Mirelly Dos Santos Pereira <mirellydevj@gmail.com>
2026-03-01 15:05:26 -0300

    Add files via upload

---
+++ portt.js
@@ -0,0 +1,13 @@
+const sections = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+sections.forEach(section => observer.observe(section));
